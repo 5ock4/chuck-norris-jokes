1684ac5d07423bf6cd8e3b632d1dcd67f37a4165
Adam Strakoš <strakosadam@gmail.com>
2021-02-15 01:36:25 +0100

    improved tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,44 @@
 import React from "react"
-import { render, fireEvent, screen } from "./shared/utils/testUtils"
-
-import App from "./App"
+import { render, screen } from "./shared/utils/testUtils"
+import userEvent from "@testing-library/user-event"
 import flushPromises from "flush-promises"
 
-jest.mock("./jokes/services/chuckNorrisAPI");
+import App from "./App"
 
-it("Displays random joke and categories", async () => {
+jest.mock("./jokes/services/chuckNorrisAPI")
+
+beforeEach(async () => {
   render(<App />)
   await flushPromises()
-  // console.log(screen.debug())
+})
+
+it("Displays joke and categories.", async () => {
   expect(screen.getByText(process.env.JOKE_RANDOM)).toBeInTheDocument()
+
+  userEvent.click(screen.getByText(process.env.JOKE_RANDOM))
+  await flushPromises()
+
+  expect(screen.getByText(process.env.JOKE_RANDOM)).toBeInTheDocument()
+  expect(screen.getByText(process.env.CAT1)).toBeInTheDocument()
+  expect(screen.getByText(process.env.CAT2)).toBeInTheDocument()
+})
+
+it("Displays correct jokes after clicking category.", async () => {
+  userEvent.click(screen.getByText(process.env.CAT1))
+  await flushPromises()
+
+  expect(screen.getByText(process.env.JOKE_CATEGORY_RANDOM)).toBeInTheDocument()
+
+  userEvent.click(screen.getByText(process.env.CAT1))
+  await flushPromises()
+
+  expect(screen.getByText(process.env.JOKE_RANDOM)).toBeInTheDocument()
+})
+
+it("Displays correct jokes after adding phrase.", async () => {
+  userEvent.type(screen.getByLabelText('Find joke with phrase'), process.env.PHRASE)
+  await flushPromises()
+
+  expect(screen.getByText(new RegExp(process.env.JOKE_SEARCH_QUERRY))).toBeInTheDocument()
+  expect(screen.getByText(new RegExp(process.env.PHRASE))).toBeInTheDocument()
 })
--- src/jokes/services/__mocks__/chuckNorrisAPI.js
+++ src/jokes/services/__mocks__/chuckNorrisAPI.js
@@ -1,13 +1,13 @@
 export const fetchJoke = async (category, searchText) => {
   if (searchText != null) {
-    return Promise.resolve(process.env.JOKE_SEARCH_QUERRY)
+    return Promise.resolve(process.env.JOKE_SEARCH_QUERRY + ":" + searchText)
   } else if (category == null) {
     return Promise.resolve(process.env.JOKE_RANDOM)
   } else {
-    return Promise.resolve(process.env.JOKE_SEARCH_QUERRY)
+    return Promise.resolve(process.env.JOKE_CATEGORY_RANDOM)
   }
 }
 
 export const fetchCategories = async () => {
-  return ["cat1", "cat2"]
+  return [process.env.CAT1, process.env.CAT2]
 }
